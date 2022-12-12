abe8ae9a58fcbdd3479859b8a9e1e067bda85ed2
vuillod <nicolas.vuillod@epfl.ch>
2022-12-12 15:51:52 +0100

    update

--- Projet_Lineaire.jl
+++ Projet_Lineaire.jl
@@ -5,13 +5,22 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ a001cc30-771e-11ed-0bc5-a37734fc6a0e
-using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
+begin 
+	using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
+	import Pkg; Pkg.add("Optim")
+end
 
 # ╔═╡ e26ffe18-fe92-48ba-859e-6e7fd5c1af11
 using DataFrames, CSV, MLJ, MLJLinearModels, Optim, MLCourse, Distributions, Plots,  Random, OpenML, Statistics, Serialization
 
 # ╔═╡ ff7be794-4b9b-4192-bc00-272548c19a3d
-clean_data = deserialize("intermediate_result.dat")
+begin 
+	clean_data = deserialize("clean_data_train.dat")
+	coerce!(clean_data, :labels => Multiclass)
+end
+
+# ╔═╡ 5d06d671-0832-4339-91d9-ec830076a2f6
+test_data = deserialize("clean_data_test.dat")
 
 # ╔═╡ 4aed2913-3743-4280-99b6-6ca841969770
 begin
@@ -21,10 +30,10 @@ end
 
 # ╔═╡ 3aaeeaa0-4fce-4c7f-8549-051b88d57393
 begin
-	solver = MLJLinearModels.LBFGS(optim_options = Optim.Options(time_limit = 60))
+	solver = MLJLinearModels.LBFGS(optim_options = Optim.Options(time_limit = 300))
 	model1 = LogisticClassifier(penalty = :l2, lambda = 1e-4, solver = solver)
 	clean_data_train.labels = categorical(clean_data_train.labels, levels=["KAT5","eGFP","CBP"], ordered = true)
-    m = machine(model1, select(clean_data_train,Not(:labels)), clean_data_train.labels)
+    m = machine(model1, select(clean_data_train,Not(:labels)),clean_data_train.labels)
     fit!(m, verbosity = 0);
 end
 
@@ -57,25 +66,30 @@ losses(m, select(clean_data_train, Not(:labels)), clean_data_train.labels)
 # ╔═╡ fe0eaa78-7836-4913-9cd0-1597cbdba8e6
 losses(m, select(clean_data_test, Not(:labels)), clean_data_test.labels)
 
-# ╔═╡ a4789350-e349-47ff-8b38-2363eb49aa6d
-begin
-    model = RidgeRegressor()
-    self_tuning_model = TunedModel(model = model, # the model to be tuned
-                                   resampling = CV(nfolds = 100), # how to evaluate
-                                   range = range(model, :lambda, scale = :log10,
-									       lower = 1e-30, upper = 1e-1), #seebelow
-                                   measure = accuracy) # evaluation measure
-	
-    self_tuning_mach = machine(self_tuning_model, select(clean_data_train, 			 
-    Not(:labels)), 	clean_data_train.labels)
-	
-	fit!(self_tuning_mach, verbosity = 0)
+# ╔═╡ 899754cf-7d00-4633-a83f-fcc973e8fc0e
+function tune_model_labels(model, data,)
+	tuned_model = TunedModel(model = model,
+	                         resampling = CV(nfolds = 10),
+	                         tuning = Grid(goal=10),
+	                         range = range(model, :lambda,
+									       scale = :log10,
+									       lower = 1e-30, upper = 1e-1),
+	                         measure = accuracy)
+	self_tuned_mach = machine(tuned_model, select(data, Not(:labels)), data.labels)
+	fit!(self_tuned_mach, verbosity = 2)
 end
+
+# ╔═╡ 78fba11f-db27-4282-895a-f528b54980d1
+ridge_res = tune_model_labels(LogisticClassifier(penalty = :l2, solver = solver), clean_data)
+
+# ╔═╡ 37ec27ef-2489-4832-9ce9-0aa192513058
+fitted_params(ridge_res)
 
 # ╔═╡ Cell order:
 # ╠═a001cc30-771e-11ed-0bc5-a37734fc6a0e
 # ╠═e26ffe18-fe92-48ba-859e-6e7fd5c1af11
 # ╠═ff7be794-4b9b-4192-bc00-272548c19a3d
+# ╠═5d06d671-0832-4339-91d9-ec830076a2f6
 # ╠═4aed2913-3743-4280-99b6-6ca841969770
 # ╠═3aaeeaa0-4fce-4c7f-8549-051b88d57393
 # ╠═075a79cb-78ff-4f30-b66c-97916599794d
@@ -83,4 +97,6 @@ end
 # ╠═2d777b75-0fcf-407b-8cf3-76599ca91275
 # ╠═b3151845-b17c-4e01-b89f-0143f152aea9
 # ╠═fe0eaa78-7836-4913-9cd0-1597cbdba8e6
-# ╠═a4789350-e349-47ff-8b38-2363eb49aa6d
+# ╠═899754cf-7d00-4633-a83f-fcc973e8fc0e
+# ╠═78fba11f-db27-4282-895a-f528b54980d1
+# ╠═37ec27ef-2489-4832-9ce9-0aa192513058
--- Projet_clean_data.jl
+++ Projet_clean_data.jl
@@ -8,10 +8,13 @@ using InteractiveUtils
 using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 
 # ╔═╡ 0105ef37-4764-409f-a8cb-1b6731af49ca
-using DataFrames, CSV, MLJ, MLJLinearModels, MLCourse, Distributions, Plots,  Random, OpenML, Statistics, Serialization
+using DataFrames, CSV, MLJ, MLJLinearModels, MLCourse, Distributions, Plots,  Random, OpenML, Statistics, Serialization, MLJMultivariateStatsInterface
 
 # ╔═╡ fe75abdd-33a2-444a-86f6-23f1884f091c
 data_init = CSV.read(pwd() * "\\train.csv", DataFrame)
+
+# ╔═╡ 80136d53-d343-4f6a-bd67-d2824ef6d70e
+test_init = CSV.read(pwd() * "\\test.csv", DataFrame)
 
 # ╔═╡ 17978cb9-6c8c-4001-b817-1c74e1d5ceda
 #---------------------------------------------------------------------------
@@ -19,18 +22,20 @@ data_init = CSV.read(pwd() * "\\train.csv", DataFrame)
 
 # ╔═╡ 43a437a7-310c-46c1-93ca-fc522405d864
 begin
-	data_without_labels = select(data_init, Not(:labels))
-	labels = select(data_init, :labels)
+	train_data_without_labels = select(data_init, Not(:labels))
+	train_labels = select(data_init, :labels)
 end
 
 # ╔═╡ bc909244-39e8-4cac-aecb-fadccc341d53
-new_data_without_labels = data_without_labels[:, std.(eachcol(data_without_labels)) .!= 0]
+#CONSTANT PREDICTORS :
+new_train_data_without_labels = train_data_without_labels[:, std.(eachcol(train_data_without_labels)) .!= 0]
 
-# ╔═╡ 9c306784-9b0a-4f87-a478-87370409783d
-size(new_data_without_labels)
+# ╔═╡ 006a81e3-4a27-4789-8c0b-999d01283865
+clean_data_test1 = test_init[:, std.(eachcol(train_data_without_labels)) .!= 0]
 
 # ╔═╡ 13da3afc-fe7b-4897-8deb-d87ec7fa796a
-tab = findall(≈(1), cor(Matrix(new_data_without_labels))) |> 
+#CORRELATE PREDICTORS :
+tab = findall(≈(1), cor(Matrix(new_train_data_without_labels))) |> 
 idxs -> filter(x -> x[1] > x[2], idxs)
 
 # ╔═╡ bddd5feb-cda1-44fd-bd1d-40e2833c122e
@@ -46,25 +51,43 @@ begin
 end
 
 # ╔═╡ ef96207b-2bf5-4433-a538-006a7d0e2e0b
-new_data_without_labels2 = select(new_data_without_labels, Not(unique(new_t)))
+begin
+	new_train_data_without_labels2 = select(new_train_data_without_labels, Not(unique(new_t)))
+	clean_data_test2 = select(clean_data_test1, Not(unique(new_t)))
+end
+
+# ╔═╡ c3ff62b1-2fb9-4604-83ef-90eaf70f0606
+#---------------------------------------------------------------------------------
+#PCA
+
+# ╔═╡ ebf40c1e-4f3d-442f-972e-74f177e0188a
+begin
+	pca_train_data_wl = fit!(machine(PCA(variance_ratio = 0.93), new_train_data_without_labels2), verbosity = 0)
+end
 
 # ╔═╡ 4829f515-7bec-4ef2-a5cd-50637ee2c711
-clean_data = hcat(new_data_without_labels2, labels)
+clean_data_train = hcat(new_train_data_without_labels2, train_labels)
 
 # ╔═╡ be344bfb-75d9-4d34-b46e-b6b770afff1f
-serialize("intermediate_result.dat", clean_data)
+begin
+	serialize("clean_data_train.dat", clean_data_train)
+	serialize("clean_data_test.dat", clean_data_test2)
+end
 
 # ╔═╡ Cell order:
 # ╠═d40d9730-7092-11ed-337f-dff4b950f6df
 # ╠═0105ef37-4764-409f-a8cb-1b6731af49ca
 # ╠═fe75abdd-33a2-444a-86f6-23f1884f091c
+# ╠═80136d53-d343-4f6a-bd67-d2824ef6d70e
 # ╠═17978cb9-6c8c-4001-b817-1c74e1d5ceda
 # ╠═43a437a7-310c-46c1-93ca-fc522405d864
 # ╠═bc909244-39e8-4cac-aecb-fadccc341d53
-# ╠═9c306784-9b0a-4f87-a478-87370409783d
+# ╠═006a81e3-4a27-4789-8c0b-999d01283865
 # ╠═13da3afc-fe7b-4897-8deb-d87ec7fa796a
 # ╠═bddd5feb-cda1-44fd-bd1d-40e2833c122e
 # ╠═53c3b744-d272-430b-8745-fc0cd2e4ae03
 # ╠═ef96207b-2bf5-4433-a538-006a7d0e2e0b
+# ╠═c3ff62b1-2fb9-4604-83ef-90eaf70f0606
+# ╠═ebf40c1e-4f3d-442f-972e-74f177e0188a
 # ╠═4829f515-7bec-4ef2-a5cd-50637ee2c711
 # ╠═be344bfb-75d9-4d34-b46e-b6b770afff1f
