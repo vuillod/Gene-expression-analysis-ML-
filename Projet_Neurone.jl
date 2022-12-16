45f01313b47591a35c0b1e3a0f99d5dba19b2a39
vuillod <nicolas.vuillod@epfl.ch>
2022-12-16 17:57:02 +0100

    Update_Neurone

--- Projet_Neurone.jl
+++ Projet_Neurone.jl
@@ -7,7 +7,7 @@ using InteractiveUtils
 # ╔═╡ 476fa2e0-77c3-11ed-38cd-35798ab207b6
 begin 
 	using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-	import Pkg; Pkg.add("Optim")
+	import Pkg; Pkg.add("MLJFlux")
 end
 
 # ╔═╡ 23c162c2-93fd-4cd6-8bf3-43ce14e2a67f
@@ -25,22 +25,42 @@ test_data = deserialize("clean_data_test.dat")
 # ╔═╡ 3778363e-9b22-4197-8aec-943f1224f0a7
 size(test_data)
 
+# ╔═╡ e954a626-e3e5-43b6-ba44-44a66f1b3f7e
+begin
+	mutable struct mybuilder <: MLJFLux.Builder 
+		n_hidden1::Int 
+		n_hidden2::Int
+		dropout::Float64
+		σ
+	end 
+	mybuilder(; n_hidden1 = 10, n_hidden2 = 10, dropout = 0.5, σ = Flux.relu) = mybuilder(n_hidden1, n_hidden2, dropout, σ)
+	function MLJFlux.build(builder::mybuilder, rng, n_in, n_out)
+		init = MLJFlux.glorot_uniform(rng)
+		return Flux.Chain(
+			Flux.Dense(n_in, builder.n_hidden1, builder.σ, init = init),
+			Flux.Dropout(builder.dropout),
+			Flux.Dense(builder.n_hidden1, builder.n_hidden2, builder.σ, init = init), 
+			Flux.Dropout(builder.dropout), 
+			Flux.Dense(builder.n_hidden2, n_out, init = init))
+	end
+end
+	
+
 # ╔═╡ 9eb88425-48ad-45af-b564-031bcbd111e1
 begin
-	model2 = NeuralNetworkClassifier(builder = MLJFlux.Short(σ = relu),
+	model2 = NeuralNetworkClassifier(builder = mybuilder(n_hidden1 = 300, n_hidden2 = 											50, dropout = 0.5, σ=Flux.relu),
     								optimiser = ADAM(),
-                             		batch_size = 200)
+                             		batch_size = 32, 
+									alpha = 1)
                        
 	tuned_model = TunedModel(model = model2,
-							  resampling = CV(nfolds = 10),
+							  resampling = Holdout(fraction_train = 0.7),
 	                          range = [range(model2,
-						                :(neural_network_classifier.builder.dropout),
-									    values = [0, .1, .2]),
+						                :lambda,
+							  			scale = :log10, lower = 1e-12, upper = 1e-6)
 								       range(model2,
-									     :(neural_network_classifier.epochs),
-									     values = [500, 1000]),
-							  		range(model2, 
-									:(neural_network_classifier.builder.n_hidden), values = [50, 150])],
+									     :epochs,
+									     values = [250, 500, 1000])],
 	                          measure = accuracy)
 	neuron_mach = fit!(machine(tuned_model,
 	                     select(clean_data, Not(:labels)), verbosity = 0))
@@ -61,12 +81,9 @@ begin
 	for i in 1:3093
 		push!(index,i)
 	end
-	kaggle_neuron = DataFrame(id=index[:], prediction = ridge_predict)
-	CSV.write(pwd()*"\\res_predictions_neuron.csv",kaggle_neuron)
+	kaggle_neuron = DataFrame(id=index[:], prediction = neuron_res)
+	CSV.write(pwd()*"\\res_predictions_neuron.csv", kaggle_neuron)
 end
-
-# ╔═╡ 3010ebcd-5bab-4413-b0ca-773daf7a6504
-
 
 # ╔═╡ Cell order:
 # ╠═476fa2e0-77c3-11ed-38cd-35798ab207b6
@@ -74,9 +91,9 @@ end
 # ╠═f1b881c4-5798-4043-af58-b0cbd1c7096e
 # ╠═509646d3-018e-4f51-992f-0ad1aee2b49e
 # ╠═3778363e-9b22-4197-8aec-943f1224f0a7
+# ╠═e954a626-e3e5-43b6-ba44-44a66f1b3f7e
 # ╠═9eb88425-48ad-45af-b564-031bcbd111e1
 # ╠═b41d3380-e157-4587-9a44-1cbc56ce5939
 # ╠═6044a3c6-33b3-40af-9c8d-66c9ee14b24e
 # ╠═7b31c8ec-1293-48fb-8aba-80a947a974c0
 # ╠═72975521-b08e-46db-bdff-a13cf93e752d
-# ╠═3010ebcd-5bab-4413-b0ca-773daf7a6504
