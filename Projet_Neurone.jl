de19b0dfe9e32af8e84972d994bfb16ff1aaa05e
vuillod <nicolas.vuillod@epfl.ch>
2022-12-17 18:05:36 +0100

    Update_one_layer

--- Projet_Neurone.jl
+++ Projet_Neurone.jl
@@ -11,7 +11,7 @@ begin
 end
 
 # ╔═╡ 23c162c2-93fd-4cd6-8bf3-43ce14e2a67f
-using DataFrames, CSV, MLJ, MLJLinearModels, Optim, MLCourse, Distributions, Plots,  Random, OpenML, Statistics, Serialization, MLJFlux, Flux, OpenML
+using DataFrames, CSV, MLJ, MLJLinearModels, Optim, MLCourse, Distributions, Plots,  Random, OpenML, Statistics, Serialization, MLJFlux, Flux
 
 # ╔═╡ f1b881c4-5798-4043-af58-b0cbd1c7096e
 begin 
@@ -26,6 +26,7 @@ test_data = deserialize("clean_data_test.dat")
 size(test_data)
 
 # ╔═╡ e954a626-e3e5-43b6-ba44-44a66f1b3f7e
+"""
 begin
 	mutable struct mybuilder <: MLJFLux.Builder 
 		n_hidden1::Int 
@@ -44,9 +45,10 @@ begin
 			Flux.Dense(builder.n_hidden2, n_out, init = init))
 	end
 end
-	
+"""
 
 # ╔═╡ 9eb88425-48ad-45af-b564-031bcbd111e1
+"""
 begin
 	model2 = NeuralNetworkClassifier(builder = mybuilder(n_hidden1 = 300, n_hidden2 = 											50, dropout = 0.5, σ=Flux.relu),
     								optimiser = ADAM(),
@@ -64,6 +66,33 @@ begin
 	                          measure = accuracy)
 	neuron_mach = fit!(machine(tuned_model,
 	                     select(clean_data, Not(:labels)), verbosity = 0))
+end
+"""
+
+# ╔═╡ 931500c4-7b09-4c1e-ae9b-4837b539bb6c
+
+
+# ╔═╡ 83c54600-2bcb-441d-ab0b-54b67e5372c3
+begin 
+	model2 = NeuralNetworkClassifier(builder =  MLJFlux.Short(n_hidden = 300, 
+															dropout = 0.6,
+                                                      σ = relu),
+    								optimiser = ADAM(),
+                             		batch_size = 50, 
+									alpha = 1)
+	
+	tuned_model = TunedModel(model = model2,
+							  resampling = Holdout(fraction_train = 0.8),
+	                          range = [range(model2,
+						                :lambda,
+							  			scale = :log10, lower = 1e-8, upper = 1e-7)
+								       range(model2,
+									     :epochs,
+									     values = [500])],
+	                          measure = accuracy)
+	neuron_mach = machine(tuned_model,
+	                     select(clean_data, Not(:labels)), clean_data.labels)
+	fit!(neuron_mach, verbosity = 2)
 end
 
 # ╔═╡ b41d3380-e157-4587-9a44-1cbc56ce5939
@@ -93,6 +122,8 @@ end
 # ╠═3778363e-9b22-4197-8aec-943f1224f0a7
 # ╠═e954a626-e3e5-43b6-ba44-44a66f1b3f7e
 # ╠═9eb88425-48ad-45af-b564-031bcbd111e1
+# ╠═931500c4-7b09-4c1e-ae9b-4837b539bb6c
+# ╠═83c54600-2bcb-441d-ab0b-54b67e5372c3
 # ╠═b41d3380-e157-4587-9a44-1cbc56ce5939
 # ╠═6044a3c6-33b3-40af-9c8d-66c9ee14b24e
 # ╠═7b31c8ec-1293-48fb-8aba-80a947a974c0
