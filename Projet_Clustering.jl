05da0c3e132b9941a84c79cb887ee298bf027c99
vuillod <nicolas.vuillod@epfl.ch>
2022-12-23 16:54:02 +0100

    Vfinal

--- Projet_Clustering.jl
+++ Projet_Clustering.jl
@@ -8,12 +8,11 @@ using InteractiveUtils
 using Pkg;Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
 
 # ╔═╡ 9400c714-064d-450b-bc0e-90b84a597f89
-using MLCourse, MLJ, DataFrames, MLJMultivariateStatsInterface, OpenML,
-          LinearAlgebra, Statistics, Random, MLJClusteringInterface, StatsPlots,
-          Distributions, Distances, Serialization
+using DataFrames, CSV, MLJ, MLJLinearModels, Optim, MLCourse, Distributions, Plots, Random, OpenML, Statistics, Serialization, MLJClusteringInterface, StatsPlots, Distributions, Distances, LinearAlgebra, TSne, MLJMultivariateStatsInterface
 
-# ╔═╡ cd5f716a-5122-426a-8998-709313a519f4
-begin 
+
+# ╔═╡ cfebb63f-5f38-4de7-8c91-7a6579a6b723
+begin
 	clean_data = deserialize("clean_data_train.dat")
 	coerce!(clean_data, :labels => Multiclass)
 end
@@ -21,33 +20,49 @@ end
 # ╔═╡ 9f8540f4-db37-4d4b-adf5-0262ec58fccd
 test_data = deserialize("clean_data_test.dat")
 
-# ╔═╡ 390f32b4-d1bf-4a9b-9209-fc02c6df31ab
-#KMeans :
-
+# ╔═╡ a851159d-b4a1-492c-a229-9c5cc29a8cb1
+#TSne visualisation :
 begin
-	KMeans_mach = machine(KMeans(k = 3), select(clean_data, Not(:labels)), clean_data.labels)
+	tsne_proj = tsne(Array(select(clean_data, Not(:labels))), 2, 0, 2000, 50.0, progress = false)
+	scatter(tsne_proj[:, 1], tsne_proj[:, 2],
+        c = Int.(int(clean_data.labels)),
+        xlabel = "TSne 1", ylabel = "TSne 2",
+        legend = false)
+
+end
+
+# ╔═╡ bc7c438c-1af4-4377-b810-88f962a5d573
+#KMeans :
+begin
+	KMeans_mach = machine(KMeans(k = 3), select(clean_data, Not(:labels)))
     fit!(KMeans_mach, verbosity = 0)
 end
 
 # ╔═╡ e8c8d460-4b9d-4a99-9c7a-6712ef4035be
-KMeans_predict = predict_mode(KMeans_mach, test_data)
+KMeans_predict = MLJ.predict(KMeans_mach, select(clean_data, Not(:labels)))
 
-# ╔═╡ 551a5788-2553-4b1e-ac65-fe355e9a5b9c
-#Kaggle KMeans :
-begin
-	index = []
-	for i in 1:3093
-		push!(index,i)
-	end
-	kaggle_KMeans = DataFrame(id=index[:], prediction = KMeans_predict)
-	CSV.write(pwd()*"\\res_predictions_KMeans.csv", kaggle_KMeans)
+# ╔═╡ 82f1d02f-86eb-4212-9dc4-e1aaedcc2c9c
+confusion_matrix(KMeans_predict, clean_data.labels)
+
+# ╔═╡ 76764df6-e5ef-4864-8c25-4d29401ecd3e
+#DBSCAN
+begin 
+	Random.seed!(0)
+	dbscan_mach = machine(DBSCAN(min_cluster_size = 50, radius = 0.5))
+	DBSCAN_pred = predict(dbscan_mach, select(clean_data, Not(:labels)))
 end
+
+# ╔═╡ 166d703b-17c0-41b0-814b-5eaa2161608b
+confusion_matrix(DBSCAN_pred, clean_data.labels)
 
 # ╔═╡ Cell order:
 # ╠═bd9df9b0-7c9d-11ed-33ca-f317e29085fa
 # ╠═9400c714-064d-450b-bc0e-90b84a597f89
-# ╠═cd5f716a-5122-426a-8998-709313a519f4
+# ╠═cfebb63f-5f38-4de7-8c91-7a6579a6b723
 # ╠═9f8540f4-db37-4d4b-adf5-0262ec58fccd
-# ╠═390f32b4-d1bf-4a9b-9209-fc02c6df31ab
+# ╠═a851159d-b4a1-492c-a229-9c5cc29a8cb1
+# ╠═bc7c438c-1af4-4377-b810-88f962a5d573
 # ╠═e8c8d460-4b9d-4a99-9c7a-6712ef4035be
-# ╠═551a5788-2553-4b1e-ac65-fe355e9a5b9c
+# ╠═82f1d02f-86eb-4212-9dc4-e1aaedcc2c9c
+# ╠═76764df6-e5ef-4864-8c25-4d29401ecd3e
+# ╠═166d703b-17c0-41b0-814b-5eaa2161608b
